fa44b8f2765722687d6ef739be6dc13c65684fd9
pking <pking@xojetaviation.com>
2023-10-26 16:34:41 -0400

    use handshake headers for test

--- tests/ws_chrome_header_test.py
+++ tests/ws_chrome_header_test.py
@@ -13,7 +13,8 @@ def test_connect_route_should_have_auth_header_passed_back_to_client_from_handsh
     # create connection
     ws = create_connection(base_url_ws_api)
     # get headers && standardize keys to lower
-    headers = dict((k.lower(), v) for k, v in ws.headers.items())
+    headers = dict((k.lower(), v) for k, v in ws.handshake_response.headers.items())
+    # headers = dict((k.lower(), v) for k, v in ws.headers.items())
     # close connection
     ws.close()
 
