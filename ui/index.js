d94d05a411996defe52a381c9816a4023ea4e45d
pking <pking@xojetaviation.com>
2023-11-03 13:26:14 -0400

    update

--- ui/index.js
+++ ui/index.js
@@ -1,19 +1,14 @@
 $.getJSON("./pulumi_output.json", function (json) {
 
-    const hasConnection = ()=>{
-        if (!socket){
+    const hasConnection = () => {
+        if (!socket) {
             alert("connect button must be pressed");
             return false;
         }
         return true
     }
 
-    let ws_url = json.wsApiUrl
-    let socket = undefined;
-    console.log(ws_url)
-
-    $("#ws-connect").click(()=>{
-        socket = new WebSocket(ws_url);
+    const socketEventListener = () => {
         // Connection opened
         socket.addEventListener("open", (event) => {
             alert("[open] Connection established");
@@ -40,11 +35,30 @@ $.getJSON("./pulumi_output.json", function (json) {
             console.log(error)
             alert(`[error]`);
         });
+    }
 
+    let ws_url = json.wsApiUrl
+    let socket = undefined;
+    // console.log(ws_url)
+
+    $("#ws-connect-with-token").click(() => {
+        if (socket) {
+            socket.close();
+        }
+        socket = new WebSocket(ws_url, ["eyJ0eXAiOiJqd3QiLCJhbGciOiJSUzI1NiIsImtpZCI6Il9XQUdTUjlrc3lqQ2drMHJoaW9rbUFxRlhoUWc1RnpteHZRNFg4b2p0VGcifQ.eyJpc3MiOiJodHRwOi8vaG9zdC5kb2NrZXIuaW50ZXJuYWw6MzAwMS8iLCJzdWIiOiJzYW1scHxNeUF6dXJlfGpvaG4uZG9lQHVua25vd24uY29tIiwiYXVkIjpbInRlc3QiLCJodHRwOi8vaG9zdC5kb2NrZXIuaW50ZXJuYWw6MzAwMS91c2VyaW5mbyJdLCJpYXQiOjE2OTkwMzA4MTYsImV4cCI6MTY5OTExNzIxNiwiYXpwIjoiIiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCIsInBlcm1pc3Npb25zIjpbImp1c3RfZG9faXQucnVuIl19.eTzdENmWMRQl3-08DpQJwi6gg-raIJQ_-58JKduPwjjaitwk0eVcDulPs9jb0TQ8oGtpSantxyCUY-XuwMiRMI53R1kvLAv0sUoXcre8RJYC18uTeTM5NnZqQHm61wl3sX9q3A1yjMAiyLMU36p3jXeRd4ra_tv96pICjl_TyA1qtMqg4PY3sKNYrVFYHg7ythCHq_bs_ljSyuWlF17479pw4xjwnZY1icV_8SjEXPYNhTemPjXEN9A24KfFZ_gLG7xzcrCMPgwdiJXg9Tvd8ipidjI-Nq7R3mJmQ5NCH3nx39eip1L05J6-bIdC1L1Ydvz3aD9rU3weU0qcZvFN2A"]);
+        socketEventListener();
     });
 
-    $("#ws-ping").click(()=>{
-        if (!hasConnection()){
+    $("#ws-connect").click(() => {
+        if (socket) {
+            socket.close();
+        }
+        socket = new WebSocket(ws_url);
+        socketEventListener();
+    });
+
+    $("#ws-ping").click(() => {
+        if (!hasConnection()) {
             return;
         }
         socket.send(JSON.stringify({
@@ -54,16 +68,16 @@ $.getJSON("./pulumi_output.json", function (json) {
         console.log("ping pressed");
     });
 
-    $("#ws-disconnect").click(()=>{
-        if (!hasConnection()){
+    $("#ws-disconnect").click(() => {
+        if (!hasConnection()) {
             return;
         }
         socket.close();
         socket = undefined;
     });
 
-    $("#ws-default").click(()=>{
-        if (!hasConnection()){
+    $("#ws-default").click(() => {
+        if (!hasConnection()) {
             return;
         }
         socket.send({
@@ -74,8 +88,8 @@ $.getJSON("./pulumi_output.json", function (json) {
     });
 
 
-    $("#ws-route-not-matching").click(()=>{
-        if (!hasConnection()){
+    $("#ws-route-not-matching").click(() => {
+        if (!hasConnection()) {
             return;
         }
         socket.send(JSON.stringify({
@@ -85,14 +99,14 @@ $.getJSON("./pulumi_output.json", function (json) {
         console.log("route not matching pressed");
     });
 
-    $("#ws-connection-idle-timeout").click(()=>{
-        if (!hasConnection()){
+    $("#ws-connection-idle-timeout").click(() => {
+        if (!hasConnection()) {
             return;
         }
         alert("if you wait patiently for 10 min without clicking any other btns then disconnect should be called, refresh page to cancel");
         $(":button").addClass('d-none');
         var timer2 = "10:03";
-        var interval = setInterval(function() {
+        var interval = setInterval(function () {
             var timer = timer2.split(':');
             var minutes = parseInt(timer[0], 10);
             var seconds = parseInt(timer[1], 10);
@@ -103,7 +117,7 @@ $.getJSON("./pulumi_output.json", function (json) {
             $('.countdown').html(minutes + ':' + seconds);
             if (minutes < 0) clearInterval(interval);
             //check if both minutes and seconds are 0
-            if ((seconds <= 0) && (minutes <= 0)){
+            if ((seconds <= 0) && (minutes <= 0)) {
                 clearInterval(interval);
                 $(":button").removeClass('d-none');
                 $('.countdown').html("Websocket Localstack Test");
